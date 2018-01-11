05c85d03d90b7ef3b4100cb1006e5ab853bef428
Alex Rozenshteyn <rpglover64@gmail.com>
2018-01-10 20:39:35 -0800

    Trivial typechecking

--- Main.hs
+++ Main.hs
@@ -1,13 +1,23 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GADTs #-}
 module Main where
 
 data Expr = I Int | Plus Expr Expr
 
-eval :: Expr -> Int
+data TExpr a where
+  TI :: Int -> TExpr Int
+  TPlus :: TExpr Int -> TExpr Int -> TExpr Int
+
+check :: Expr -> TExpr Int
+check = \case
+  I i -> TI i
+  Plus e1 e2 -> TPlus (check e1) (check e2)
+
+eval :: TExpr a -> a
 eval = \case
-  I i -> i
-  Plus e1 e2 -> eval e1 + eval e2
+  TI i -> i
+  TPlus e1 e2 -> eval e1 + eval e2
 
 testExpr = (I 1 `Plus` I 2) `Plus` I 3
 
-main = print $ eval testExpr
+main = print $ eval $ check testExpr
